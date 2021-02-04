dfbc143c292a2328a7827ed2de5ec3ff78ca0b19
dwisecar <dwisecar@gmail.com>
2021-02-03 16:31:34 -0800

    done

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-class Timer extends Component {
+class Timer extends PureComponent {
   constructor() {
     super();
     this.timer = React.createRef();
@@ -10,7 +10,6 @@ class Timer extends Component {
     };
   }
 
-  //Your code here
 
   componentDidMount() {
     this.interval = setInterval(
@@ -18,6 +17,20 @@ class Timer extends Component {
       this.props.updateInterval * 1000
     );
   }
+
+  componentDidUpdate(){
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16); 
+  }
+
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
 
   componentWillUnmount() {
     clearInterval(this.interval);
